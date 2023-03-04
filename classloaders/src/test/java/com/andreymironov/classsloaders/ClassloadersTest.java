efa2599ccec7e0a7792c2d0a0aa4c5a0da88cf93
Andrey Mironov <mironov.a@iac.spb.ru>
2023-03-04 18:09:28 +0700

    classloaders

--- classloaders/src/test/java/com/andreymironov/classsloaders/ClassloadersTest.java
+++ classloaders/src/test/java/com/andreymironov/classsloaders/ClassloadersTest.java
@@ -1,29 +1,30 @@
 package com.andreymironov.classsloaders;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ClassloadersTest {
     @Test
     public void testBootstrapClassloader() {
         SoftAssertions softAssertions = new SoftAssertions();
 
-        Class<?> someJdkInternalClass = ArrayList.class;
-        System.out.println("Check the classloader of some JDK internal class: " + someJdkInternalClass);
+        List<Class<?>> someJdkInternalClasses = List.of(List.class, String.class, ClassLoader.class);
+        System.out.println("Check the classloader of some JDK internal classes: " + someJdkInternalClasses);
+        someJdkInternalClasses.forEach(clazz -> {
+            ClassLoader bootstrapClassloader = clazz.getClassLoader();
+            softAssertions.assertThat(bootstrapClassloader).isNull();
+        });
+
         System.out.println("""
-                It is called the bootstrap classloader and it's mainly responsible for loading
-                JDK internal classes, typically rt.jar and other core libraries located in the
-                $JAVA_HOME/jre/lib directory. Additionally, the Bootstrap class loader serves as the parent
-                of all the other ClassLoader instances.
-                """);
-        ClassLoader bootstrapClassloader = someJdkInternalClass.getClassLoader();
-        softAssertions.assertThat(bootstrapClassloader).isNull();
-        System.out.println("""
-                It is null, because the bootstrap class loader is written in native code, not Java,
-                so it doesn't show up as a Java class. As a result, the behavior of the bootstrap
-                class loader will differ across JVMs.
+                It is called the bootstrap classloader and it's mainly responsible for loading JDK internal classes,
+                typically rt.jar and other core libraries located in the $JAVA_HOME/jre/lib directory.
+                Additionally, the Bootstrap class loader serves as the parent of all the other ClassLoader instances.
+                It is null, because the bootstrap class loader is written in native code, not Java, so it doesn't
+                show up as a Java class.
+                As a result, the behavior of the bootstrap class loader will differ across JVMs.
                 """);
 
         softAssertions.assertAll();
@@ -37,9 +38,7 @@ public class ClassloadersTest {
                 to all applications running on the platform.
                 The extension class loader loads from the JDK extensions directory, usually the
                 $JAVA_HOME/lib/ext directory, or any other directory mentioned in the java.ext.dirs system property.
-                """);
-        System.out.println("""
-                The class of extension classloader is %s
+                The class of extension classloader is %s.
                 """.formatted(ClassLoader.getSystemClassLoader().getParent().getClass()));
     }
 
@@ -47,20 +46,21 @@ public class ClassloadersTest {
     public void testSystemClassloader() {
         SoftAssertions softAssertions = new SoftAssertions();
 
-        Class<?> someAppClass = this.getClass();
-        System.out.println("Check classloader of some application class: " + someAppClass);
+        List<Class<?>> someAppClasses = List.of(this.getClass(), Assertions.class, Test.class);
+        System.out.println("Check classloader of the following classes from classpath: " + someAppClasses);
+        someAppClasses.forEach(clazz -> {
+            ClassLoader appClassloader = clazz.getClassLoader();
+            softAssertions.assertThat(appClassloader).isNotNull();
+            softAssertions.assertThat(appClassloader).isEqualTo(ClassLoader.getSystemClassLoader());
+        });
+
         System.out.println("""
-                The system or application class loader, on the other hand, takes care of loading
-                all the application level classes into the JVM. It loads files found in the classpath
-                environment variable, -classpath, or -cp command line option. It's also a child of
-                the extensions class loader.
-                """);
-        ClassLoader appClassloader = someAppClass.getClassLoader();
-        softAssertions.assertThat(appClassloader).isNotNull();
-        System.out.println("""
+                The system or application class loader takes care of loading all the application level classes into the JVM.
+                It loads files found in the classpath environment variable, -classpath, or -cp command line option.
+                It's also a child of the extensions class loader.
                 The implementation is JVM specific, the name of the current JVM app classloader
-                class is %s
-                """.formatted(appClassloader.getClass().getName()));
+                class is %s.
+                """.formatted(ClassLoader.getSystemClassLoader().getClass().getName()));
 
         softAssertions.assertAll();
     }
