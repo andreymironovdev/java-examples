3672a6f33899f068b6f93a6a67ad83efa903ff01
Andrey Mironov <mironov.a@iac.spb.ru>
2023-06-11 20:11:34 +0700

    update algorithms

---
+++ algorithms/src/main/java/com/andreymironov/algorithms/AnagramUtils.java
@@ -0,0 +1,38 @@
+package com.andreymironov.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AnagramUtils {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<Integer, List<String>> map = Arrays.stream(strs)
+                .collect(Collectors.groupingBy(AnagramUtils::hashCodeWithCollisionsOnAnagrams));
+
+        return map.values().stream().toList();
+    }
+
+    private static int hashCodeWithCollisionsOnAnagrams(String row) {
+        if (row == null) {
+            return 0;
+        }
+        return row.chars()
+                .sorted()
+                .reduce(0, ((left, right) -> 31 * left + right));
+    }
+
+    /**
+     * @param s - string consists of lowercase english characters
+     * @param t - string consists of lowercase english characters
+     * @return true, if t is anagram of s
+     */
+    public static boolean isAnagram(String s, String t) {
+        int[] sFrequency = new int[26];
+        int[] tFrequency = new int[26];
+        s.chars().forEach(ch -> sFrequency[ch - 'a']++);
+        t.chars().forEach(ch -> tFrequency[ch - 'a']++);
+        return IntStream.range(0, 26).noneMatch(i -> sFrequency[i] != tFrequency[i]);
+    }
+}
---
+++ algorithms/src/test/java/com/andreymironov/algorithms/AnagramUtilsTest.java
@@ -0,0 +1,35 @@
+package com.andreymironov.algorithms;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class AnagramUtilsTest {
+    @Test
+    void should_group() {
+        Assertions.assertThat(AnagramUtils.groupAnagrams(new String[]{"act", "god", "dog", "cat"}))
+                .containsExactlyInAnyOrder(List.of("act", "cat"), List.of("god", "dog"));
+    }
+
+    @Test
+    void should_group_array_with_collisions_for_basic_sum_hash() {
+        Assertions.assertThat(AnagramUtils.groupAnagrams(new String[]{"duh","ill"}))
+                .containsExactlyInAnyOrder(List.of("ill"), List.of("duh"));
+    }
+
+    @Test
+    void should_see_the_anagram() {
+        Assertions.assertThat(AnagramUtils.isAnagram("anagram", "nagaram")).isTrue();
+    }
+
+    @Test
+    void should_not_see_the_anagram_1() {
+        Assertions.assertThat(AnagramUtils.isAnagram("car", "rat")).isFalse();
+    }
+
+    @Test
+    void should_not_see_the_anagram_2() {
+        Assertions.assertThat(AnagramUtils.isAnagram("aac", "bbc")).isFalse();
+    }
+}
